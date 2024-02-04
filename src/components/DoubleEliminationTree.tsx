cbd0b2328181ce046b96d1dc6d02c2f41f26a13c
Thomas Kellermeier <thomas.kellermeier@contentful.com>
2024-02-04 17:50:43 +0100

    feat: render double elimination tree

--- src/components/DoubleEliminationTree.tsx
+++ src/components/DoubleEliminationTree.tsx
@@ -14,14 +14,63 @@ type DoubleElimininationTreeProps = {
   players: PlayerEntry[];
 };
 
-const toVisualMatch = (match: MatchEntry, players: PlayerEntry[]) => {
+type MatchesMap = {
+  upper: {
+    [roundIndex: number]: {
+      [matchIndex: number]: MatchEntry;
+    };
+  };
+  lower: {
+    [roundIndex: number]: {
+      [matchIndex: number]: MatchEntry;
+    };
+  };
+};
+
+const toVisualMatch = (
+  match: MatchEntry,
+  players: PlayerEntry[],
+  matchesMap: MatchesMap
+) => {
+  const round = match.fields.round["en-US"];
+  const roundMatchIndex = match.fields.roundMatchIndex["en-US"];
+  const isUpper = match.fields.isUpperBracket["en-US"];
+  const bracket = isUpper ? matchesMap.upper : matchesMap.lower;
+  const nextRoundMatchIndex =
+    isUpper || round % 2 === 0
+      ? Math.ceil(roundMatchIndex / 2)
+      : roundMatchIndex;
+  let nextMatch =
+    bracket[match.fields.round["en-US"] + 1]?.[nextRoundMatchIndex];
+  if (!isUpper && !nextMatch) {
+    const lastRound = Object.keys(matchesMap.upper).length;
+    // The last lower bracket match points again to the final in the upper bracket at the end
+    nextMatch = matchesMap.upper[lastRound]?.[1];
+  }
+
+  let nextLoserMatch: MatchEntry | null = null;
+  if (isUpper && !match.fields.matchNumber["en-US"]?.startsWith("Final")) {
+    const nextLoserMatchRound = Math.max(1, 2 * (round - 1)); // 1>1, 2>2, 3>4, 4>6, 5>8
+    const amountLoserRoundMatches = Object.values(
+      matchesMap.lower[nextLoserMatchRound]
+    ).length;
+    const nextLoserMatchRoundIndex =
+      round === 1
+        ? Math.ceil(roundMatchIndex / 2)
+        : round % 2 === 0
+        ? amountLoserRoundMatches - roundMatchIndex + 1
+        : roundMatchIndex;
+    nextLoserMatch =
+      matchesMap.lower[nextLoserMatchRound][nextLoserMatchRoundIndex];
+  }
+
   return {
     id: match.sys.id,
     name: match.fields.matchNumber["en-US"],
-    nextMatchId: null, // TODO:
-    nextLooserMatchId: null, // TODO:
+    nextMatchId: nextMatch?.sys.id ?? null,
+    nextLooserMatchId: nextLoserMatch?.sys.id ?? null,
     tournamentRoundText: match.fields.round["en-US"],
-    startTime: "00:00",
+    startTime: null,
     state: null,
     // TODO: href
     participants: [
@@ -43,6 +92,23 @@ const toVisualMatch = (match: MatchEntry, players: PlayerEntry[]) => {
   };
 };
 
+export const createMatchesMap = (matches: MatchEntry[]): MatchesMap => {
+  const matchesMap: MatchesMap = {
+    upper: {},
+    lower: {},
+  };
+  matches.forEach((match) => {
+    const round = match.fields.round["en-US"];
+    const isUpper = match.fields.isUpperBracket["en-US"];
+    const bracket = isUpper ? matchesMap.upper : matchesMap.lower;
+    if (!bracket[round]) {
+      bracket[round] = {};
+    }
+    bracket[round][match.fields.roundMatchIndex["en-US"]] = match;
+  });
+  return matchesMap;
+};
+
 export const DoubleElimininationTree = ({
   tournament,
   matches,
@@ -53,19 +119,23 @@ export const DoubleElimininationTree = ({
       matches.find((match) => match.sys.id === matchLink.sys.id)
     )
     .filter((match): match is MatchEntry => !!match);
+  const tournamentMatchesMap = createMatchesMap(tournamentMatches);
 
   const upperBracketMatches = tournamentMatches
     .filter((match) => match.fields.isUpperBracket["en-US"])
-    .map((match) => toVisualMatch(match, players));
+    .map((match) => toVisualMatch(match, players, tournamentMatchesMap));
 
   const lowerBracketMatches = tournamentMatches
     .filter((match) => !match.fields.isUpperBracket["en-US"])
-    .map((match) => toVisualMatch(match, players));
+    .map((match) => toVisualMatch(match, players, tournamentMatchesMap));
 
   const bracketMatches = {
     upper: upperBracketMatches,
     lower: lowerBracketMatches,
   };
+
+  // TODO: Text color player #c0c3c8
+
   return (
     <DoubleEliminationBracket
       matches={bracketMatches}
@@ -74,11 +144,17 @@ export const DoubleElimininationTree = ({
       // onMatchClick={(match) => console.log(match)}
       // onPartyClick={(party) => console.log(party)}
       // theme, options
-      svgWrapper={({ children, ...props }: any) => (
-        <SVGViewer width={500} height={500} {...props}>
-          {children}
-        </SVGViewer>
-      )}
+      svgWrapper={({ children, ...props }: any) => {
+        if (tournament.fields.players["en-US"].length <= 4) {
+          return <>{children}</>;
+        } else {
+          return (
+            <SVGViewer width={1000} height={1000} {...props}>
+              {children}
+            </SVGViewer>
+          );
+        }
+      }}
     />
   );
 };
