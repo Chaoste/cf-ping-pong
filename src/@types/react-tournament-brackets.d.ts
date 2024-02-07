70b2ae655e2e1d8ae4284c79ea290b10b6d9ea63
Thomas Kellermeier <thomas.kellermeier@contentful.com>
2024-02-07 23:28:32 +0100

    feat: update next matches automatically

---
+++ src/@types/react-tournament-brackets.d.ts
@@ -0,0 +1,177 @@
+declare module "@g-loot/react-tournament-brackets";
+
+// Copied from node_modules/@g-loot/react-tournament-brackets/dist/cjs/types.d.ts
+declare module "@g-loot/react-tournament-brackets" {
+  export type ParticipantType = {
+    id: string | number;
+    isWinner?: boolean;
+    name?: string;
+    status?: "PLAYED" | "NO_SHOW" | "WALK_OVER" | "NO_PARTY" | string | null;
+    resultText?: string | null;
+    [key: string]: any;
+  };
+  export type MatchType = {
+    id: number | string;
+    href?: string;
+    name?: string;
+    nextMatchId: number | string | null;
+    nextLooserMatchId?: number | string;
+    tournamentRoundText?: string;
+    startTime: string;
+    state: "PLAYED" | "NO_SHOW" | "WALK_OVER" | "NO_PARTY" | string;
+    participants: ParticipantType[];
+    [key: string]: any;
+  };
+  export type OptionsType = {
+    width?: number;
+    boxHeight?: number;
+    canvasPadding?: number;
+    spaceBetweenColumns?: number;
+    spaceBetweenRows?: number;
+    connectorColor?: string;
+    connectorColorHighlight?: string;
+    roundHeader?: {
+      isShown?: boolean;
+      height?: number;
+      marginBottom?: number;
+      fontSize?: number;
+      fontColor?: string;
+      backgroundColor?: string;
+      fontFamily?: string;
+      roundTextGenerator?: (
+        currentRoundNumber: number,
+        roundsTotalNumber: number
+      ) => string | undefined;
+    };
+    roundSeparatorWidth?: number;
+    lineInfo?: {
+      separation?: number;
+      homeVisitorSpread?: number;
+    };
+    horizontalOffset?: number;
+    wonBywalkOverText?: string;
+    lostByNoShowText?: string;
+  };
+  export type ComputedOptionsType = OptionsType & {
+    rowHeight?: number;
+    columnWidth?: number;
+  };
+  export type SvgViewerProps = {
+    height: number;
+    width: number;
+    bracketWidth: number;
+    bracketHeight: number;
+    children: ReactElement;
+    startAt: number[];
+    scaleFactor: number;
+  };
+  export type MatchComponentProps = {
+    match: MatchType;
+    onMatchClick: (args: {
+      match: MatchType;
+      topWon: boolean;
+      bottomWon: boolean;
+      event: React.MouseEvent<HTMLAnchorElement, MouseEvent>;
+    }) => void;
+    onPartyClick: (party: ParticipantType, partyWon: boolean) => void;
+    onMouseEnter: (partyId: string | number) => void;
+    onMouseLeave: () => void;
+    topParty: ParticipantType;
+    bottomParty: ParticipantType;
+    topWon: boolean;
+    bottomWon: boolean;
+    topHovered: boolean;
+    bottomHovered: boolean;
+    topText: string;
+    bottomText: string;
+    connectorColor?: string;
+    computedStyles?: ComputedOptionsType;
+    teamNameFallback: string;
+    resultFallback: (participant: ParticipantType) => string;
+  };
+  export type ThemeType = {
+    fontFamily: string;
+    transitionTimingFunction: string;
+    disabledColor: string;
+    roundHeaders: {
+      background: string;
+    };
+    matchBackground: {
+      wonColor: string;
+      lostColor: string;
+    };
+    border: {
+      color: string;
+      highlightedColor: string;
+    };
+    textColor: {
+      highlighted: string;
+      main: string;
+      dark: string;
+      disabled: string;
+    };
+    score: {
+      text: {
+        highlightedWonColor: string;
+        highlightedLostColor: string;
+      };
+      background: {
+        wonColor: string;
+        lostColor: string;
+      };
+    };
+    canvasBackground: string;
+  };
+  export type CommonTreeProps = {
+    svgWrapper?: (props: {
+      bracketWidth: number;
+      bracketHeight: number;
+      startAt: number[];
+      children: ReactElement;
+    }) => React.ReactElement;
+    theme?: ThemeType;
+    options?: {
+      style: OptionsType;
+    };
+  };
+  export type BracketLeaderboardProps = CommonTreeProps & {
+    matchComponent: (props: MatchComponentProps) => JSX.Element;
+    currentRound?: string;
+    onMatchClick?: (args: {
+      match: MatchType;
+      topWon: boolean;
+      bottomWon: boolean;
+    }) => void;
+    onPartyClick?: (party: ParticipantType, partyWon: boolean) => void;
+  };
+  export type SingleElimLeaderboardProps = BracketLeaderboardProps & {
+    matches: MatchType[];
+  };
+  export type DoubleElimLeaderboardProps = BracketLeaderboardProps & {
+    matches: {
+      upper: MatchType[];
+      lower: MatchType[];
+    };
+  };
+  declare function SVGViewer({
+    height,
+    width,
+    bracketWidth,
+    bracketHeight,
+    children,
+    startAt,
+    scaleFactor,
+    customToolbar,
+    ...rest
+  }: {
+    [x: string]: any;
+    height?: number;
+    width?: number;
+    bracketWidth: any;
+    bracketHeight: any;
+    children: any;
+    startAt?: number[];
+    scaleFactor?: number;
+    customToolbar?: any;
+  }): import("react/jsx-runtime").JSX.Element;
+}
--- src/components/DoubleEliminationTree.tsx
+++ src/components/DoubleEliminationTree.tsx
@@ -3,10 +3,32 @@ import {
   DoubleEliminationBracket,
   Match as MatchComponent,
   SVGViewer,
-  // @ts-ignore
+  createTheme,
 } from "@g-loot/react-tournament-brackets";
 import { MatchEntry, PlayerEntry, TournamentEntry } from "../types";
 import { Link } from "contentful-management";
+import { useSDK } from "@contentful/react-apps-toolkit";
+import { PageAppSDK } from "@contentful/app-sdk";
+import { useParams } from "react-router";
+import { useQueryClient } from "react-query";
+
+const WhiteTheme = createTheme({
+  textColor: { main: "#000000", highlighted: "#07090D", dark: "#3E414D" },
+  matchBackground: { wonColor: "#daebf9", lostColor: "#96c6da" },
+  score: {
+    background: { wonColor: "#87b2c4", lostColor: "#87b2c4" },
+    text: { highlightedWonColor: "#7BF59D", highlightedLostColor: "#FB7E94" },
+  },
+  border: {
+    color: "#CED1F2",
+    highlightedColor: "#da96c6",
+  },
+  roundHeader: { fontColor: "#000" },
+  roundHeaders: { background: "#da96c6" },
+  connectorColor: "#CED1F2",
+  connectorColorHighlight: "#da96c6",
+  svgBackground: "#FAFAFA",
+});
 
 type DoubleElimininationTreeProps = {
   tournament: TournamentEntry;
@@ -40,12 +62,12 @@ const toVisualMatch = (
     isUpper || round % 2 === 0
       ? Math.ceil(roundMatchIndex / 2)
       : roundMatchIndex;
-  let nextMatch =
+  let nextMatch: MatchEntry | null =
     bracket[match.fields.round["en-US"] + 1]?.[nextRoundMatchIndex];
   if (!isUpper && !nextMatch) {
     const lastRound = Object.keys(matchesMap.upper).length;
     // The last lower bracket match points again to the final in the upper bracket at the end
-    nextMatch = matchesMap.upper[lastRound]?.[1];
+    nextMatch = matchesMap.upper[lastRound]?.[1] as MatchEntry | null;
   }
 
   let nextLoserMatch: MatchEntry | null = null;
@@ -64,6 +86,25 @@ const toVisualMatch = (
       matchesMap.lower[nextLoserMatchRound][nextLoserMatchRoundIndex];
   }
 
+  let setsWonByPlayer1 = 0;
+  let setsWonByPlayer2 = 0;
+  const sets = [
+    match.fields.resultSet1?.["en-US"],
+    match.fields.resultSet2?.["en-US"],
+    match.fields.resultSet3?.["en-US"],
+  ];
+  sets.forEach((set) => {
+    if (set) {
+      const [score1, score2] = set.split("-").map(Number);
+      if (!score1 || !score2) return;
+      if (score1 > score2) {
+        setsWonByPlayer1++;
+      } else {
+        setsWonByPlayer2++;
+      }
+    }
+  });
+
   return {
     id: match.sys.id,
     name: match.fields.matchNumber["en-US"],
@@ -78,7 +119,7 @@ const toVisualMatch = (
       match.fields.player2?.["en-US"],
     ]
       .filter((playerLink): playerLink is Link<"Entry"> => !!playerLink)
-      .map((playerLink) => {
+      .map((playerLink, index) => {
         const player = players.find(
           (player) => player.sys.id === playerLink!.sys.id
         );
@@ -86,11 +127,21 @@ const toVisualMatch = (
           id: player!.sys.id,
           name: player!.fields.name["en-US"],
           status: null,
-          // TODO: isWinner, resultText
+          // true: player 1 won, false: player 2 won, undefined: not played yet
+          isWinner: match.fields.winner?.["en-US"]
+            ? match.fields.winner["en-US"] === !Boolean(index)
+            : undefined,
+          resultText: match.fields.winner?.["en-US"]
+            ? index === 0
+              ? setsWonByPlayer1
+              : setsWonByPlayer2
+            : undefined,
         };
       }),
   };
 };
+
+type MatchType = ReturnType<typeof toVisualMatch>;
 
 export const createMatchesMap = (matches: MatchEntry[]): MatchesMap => {
   const matchesMap: MatchesMap = {
@@ -114,6 +165,9 @@ export const DoubleElimininationTree = ({
   matches,
   players,
 }: DoubleElimininationTreeProps) => {
+  const sdk = useSDK<PageAppSDK>();
+  const { tournamentId } = useParams();
+  const queryClient = useQueryClient();
   const tournamentMatches = tournament.fields.matches["en-US"]
     .map((matchLink) =>
       matches.find((match) => match.sys.id === matchLink.sys.id)
@@ -134,26 +188,105 @@ export const DoubleElimininationTree = ({
     lower: lowerBracketMatches,
   };
 
+  const openMatch = async ({ match }: { match: MatchType }) => {
+    if (!match.id) return;
+    const matchVersion = matches.find((m) => m.sys.id === match.id)?.sys
+      .version;
+    const { entity: updatedMatch } = await sdk.navigator.openEntry<
+      MatchEntry["fields"]
+    >(match.id, {
+      slideIn: {
+        waitForClose: true,
+      },
+    });
+    if (updatedMatch && updatedMatch?.sys.version !== matchVersion) {
+      const nextMatch = matches.find((m) => m.sys.id === match.nextMatchId);
+      const nextLooserMatch = matches.find(
+        (m) => m.sys.id === match.nextLooserMatchId
+      );
+      const winner = updatedMatch.fields.winner?.["en-US"];
+      if (winner !== undefined) {
+        const winnerPlayerId = winner
+          ? updatedMatch.fields.player1?.["en-US"]
+          : updatedMatch.fields.player2?.["en-US"];
+        const looserPlayerId = winner
+          ? updatedMatch.fields.player2?.["en-US"]
+          : updatedMatch.fields.player1?.["en-US"];
+        if (nextMatch) {
+          if (!nextMatch.fields.player1?.["en-US"]) {
+            nextMatch.fields.player1 = { "en-US": winnerPlayerId };
+          } else if (!nextMatch.fields.player2?.["en-US"]) {
+            nextMatch.fields.player2 = { "en-US": winnerPlayerId };
+          }
+          await sdk.cma.entry.update({ entryId: nextMatch.sys.id }, nextMatch);
+        }
+        if (nextLooserMatch) {
+          if (!nextLooserMatch.fields.player1?.["en-US"]) {
+            nextLooserMatch.fields.player1 = { "en-US": looserPlayerId };
+          } else if (!nextLooserMatch.fields.player2?.["en-US"]) {
+            nextLooserMatch.fields.player2 = { "en-US": looserPlayerId };
+          }
+          await sdk.cma.entry.update(
+            { entryId: nextLooserMatch.sys.id },
+            nextLooserMatch
+          );
+        }
+      }
+      queryClient.invalidateQueries(["tournaments", tournamentId, "matches"]);
+    }
+  };
+
+  const openParty = async (party: { id: string }) => {
+    if (!party.id) return;
+    const partyVersion = players.find((p) => p.sys.id === party.id)?.sys
+      .version;
+    const { entity: updatedParty } = await sdk.navigator.openEntry<
+      PlayerEntry["fields"]
+    >(party.id, {
+      slideIn: {
+        waitForClose: true,
+      },
+    });
+    if (updatedParty && updatedParty?.sys.version !== partyVersion) {
+      queryClient.invalidateQueries(["players"]);
+    }
+  };
+
   // TODO: Text color player #c0c3c8
 
   return (
     <DoubleEliminationBracket
       matches={bracketMatches}
       matchComponent={MatchComponent}
-      // currentRound={1}
-      // onMatchClick={(match) => console.log(match)}
-      // onPartyClick={(party) => console.log(party)}
-      // theme, options
+      onMatchClick={openMatch}
+      onPartyClick={openParty}
       svgWrapper={({ children, ...props }: any) => {
         if (tournament.fields.players["en-US"].length <= 4) {
           return <>{children}</>;
         } else {
           return (
-            <SVGViewer width={1000} height={1000} {...props}>
+            <SVGViewer
+              background={WhiteTheme.svgBackground}
+              SVGBackground={WhiteTheme.svgBackground}
+              width={1000}
+              height={1000}
+              {...props}
+            >
               {children}
             </SVGViewer>
           );
         }
+      }}
+      theme={WhiteTheme}
+      options={{
+        style: {
+          roundHeader: {
+            backgroundColor: WhiteTheme.roundHeader.backgroundColor,
+            fontColor: WhiteTheme.roundHeader.fontColor,
+          },
+          connectorColor: WhiteTheme.connectorColor,
+          connectorColorHighlight: WhiteTheme.connectorColorHighlight,
+        },
       }}
     />
   );
